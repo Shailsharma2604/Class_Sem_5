6f1fc597750bcf1ae6c0310baa0b7649ed4c80d0
Shailsharma2604 <shailsharma020604@gmail.com>
2024-07-15 12:04:04 +0530

    1

--- D.S.A/Day_1.java
+++ D.S.A/Day_1.java
@@ -1,5 +1,21 @@
 public class Day_1 {
     public static void main(String[] args) {
-        
+        int a=8;
+        float c=8f;
+        var b=0;
+        String textBlock = """
+                  Hello,
+                  World!
+                  This is a text block in Java.
+                  """;
+        System.out.println(a+" "+b+" "+c+" "+textBlock);
+
+
+        int d=0;
+        int x=6;
+        int e=d+x;
+        System.out.println(e);
+
+
     }
 }
