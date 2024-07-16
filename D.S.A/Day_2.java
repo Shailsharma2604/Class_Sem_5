ed88c2f123a7898cc6583ec9490f0f86f944f0d7
Shailsharma2604 <shailsharma020604@gmail.com>
2024-07-16 11:48:06 +0530

    1

--- D.S.A/Day_2.java
+++ D.S.A/Day_2.java
@@ -3,6 +3,17 @@ import java.util.*;
 
 public class Day_2 {
     public static void main(String[] args) {
+
         System.out.println("The complement of ~5 is: " + (~5));
+
+        System.out.println("The complement of ~ -6 is: " + (~-6));
+
+        int number_1 = 5;
+        int twoComplement_1 = ~number_1;
+        System.out.println("2's complement of ~5 is: " + twoComplement_1);
+
+        int number_2 = -6;
+        int twoComplement_2 = ~number_2;
+        System.out.println("2's complement of -6 is: " + twoComplement_2);
     }
 }
