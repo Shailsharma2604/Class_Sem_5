6a0db30f4f1cfd43b13a375e7a4e08ad7589c827
Shailsharma2604 <shailsharma020604@gmail.com>
2024-07-17 09:55:35 +0530

    1

--- D.S.A/Day_3.java
+++ D.S.A/Day_3.java
@@ -1,5 +1,31 @@
 public class Day_3 {
+    // Moved the static variables outside the main method to the class level
+    static String s1 = new String("hello");
+    static StringBuffer s2 = new StringBuffer("world");
+    static StringBuilder s3 = new StringBuilder("to java");
+
     public static void main(String[] args) {
-        
+        String s1 = "Hello";
+        System.out.println(concatenate1(s1));
+        System.out.println(s1);
+        // System.out.println(s1); // Print the result after concatenation
+        // concatenate2(new StringBuffer("2"));
+        // System.out.println(s2); // Print the result after concatenation
+        // concatenate3(new StringBuilder("3"));
+        // System.out.println(s3); // Print the result after concatenation
+    }
+
+    public static String concatenate1(String ss1) {
+        return(ss1 + ss1);
+    }
+
+    public static void concatenate2(StringBuffer ss2) {
+        // Corrected the way of appending to the StringBuffer
+        s2.insert(0, ss2);
+    }
+
+    public static void concatenate3(StringBuilder ss3) {
+        // Corrected the way of appending to the StringBuilder
+        s3.insert(0, ss3);
     }
 }
