960e2c076730807ab421ae00e3156a3ec5595c41
Shailsharma2604 <shailsharma020604@gmail.com>
2024-07-23 22:06:52 +0530

    1

---
+++ W.D/Front-end-development/day_4.js
@@ -0,0 +1,16 @@
+// // 2d Array
+// // 1. Write a JavaScript program to create a 2D array of 3 x 3 elements. The elements are 0, 1, 2, 3, 4, 5, 6, 7, 8.
+// Array2D();{
+//     var arr = [];
+//     var count = 0;
+//     for (var i = 0; i < 3; i++) {
+//         arr[i] = [];
+//         for (var j = 0; j < 3; j++) {
+//         arr[i][j] = count++;
+//         }
+//     }
+//     return arr;
+// }
+
+let Array=[[1,2,3],[4,5,6,],[7,8,9,]];
+console.log(Array);
