1642fa26d880c5eaecff497298c82fb63d7fd736
Shailsharma2604 <shailsharma020604@gmail.com>
2024-07-29 10:53:53 +0530

    1

---
+++ D.S.A/Day_4.java
@@ -0,0 +1,24 @@
+public class Day_4 {
+    public static void main(String[] args) {
+        int number = 29; // Replace with the number you want to check
+
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
