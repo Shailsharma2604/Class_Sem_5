5b0d65b069eec02974bafe20ae4ba3e5661f1b18
Shailsharma2604 <shailsharma020604@gmail.com>
2024-08-01 10:05:01 +0530

    1

---
+++ D.S.A/Day_5.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t  =Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String st[] = read.readLine().trim().split(" ");
+            int N = Integer.parseInt(st[0]);
+            int M = Integer.parseInt(st[1]);
+            
+            int arr1[] = new int[N];
+            int arr2[] = new int[M];
+            
+            st = read.readLine().trim().split(" ");
+            for(int i = 0; i < N; i++)
+              arr1[i] = Integer.parseInt(st[i]);
+            
+            st = read.readLine().trim().split(" ");  
+            for(int i = 0; i< M; i++)
+              arr2[i] = Integer.parseInt(st[i]);
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = obj.findUnion(arr1, arr2, N, M);
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println(); 
+        }
+    }
+}
+
+
+
+class Solution
+{
+    //Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        // add your code here
+        int x=0;
+        int y;
+        ArrayList<Integer> arr3 = new ArrayList<Integer>();
+        for(int i=0;i<arr1.length;i++){
+            if(arr1[i]==arr2[i]){
+                arr3.add(arr1[i]);
+            }else if(arr1[i]!=arr2[i]){
+                arr3.add(arr1[i]);
+            }
+        }
+        for(int i=0;i<arr2.length;i++){
+            if(arr2[i]!=arr3.get(i)){
+                arr3.add(arr2[i]);
+            }
+        }
+        return arr3;
+    }
+}
---
+++ W.D/Front-end-development/day_8.js
@@ -0,0 +1,9 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  greet() {
+    return `Hello, my name is ${this.name}`;
+  }
+}
