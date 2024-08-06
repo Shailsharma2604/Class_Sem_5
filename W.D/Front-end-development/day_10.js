561306ea0d9c8538e02af78cc2fe47de2ac03569
Shailsharma2604 <shailsharma020604@gmail.com>
2024-08-06 09:45:26 +0530

    1

--- W.D/Front-end-development/day_10.js
+++ W.D/Front-end-development/day_10.js
@@ -1,8 +1,29 @@
-var a=100;
+// var a=100;
 
-f1();
-function f1(){
-  console.log(a);
-  var a=200;
-  console.log("hello");
+// f1();
+// function f1(){
+//   console.log(a);
+//   var a=200;
+//   console.log("hello");
+// }
+
+// console.log(a);
+
+let o=100;     //are created in globe scope and can be accessed in any function
+var l=222;
+const p=333;
+function fun1(){
+    let a=100;
+    console.log("fun1");
+    function fun2(){
+        console.log("fun2");
+        console.log("a",a);
+        console.log("o",o);
+        console.log("l",l);
+        console.log("p",p);
+        // console.log(a);
+    }
+    fun2();
 }
+
+fun1();
