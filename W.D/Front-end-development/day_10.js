e2040576273e13c01e33299f67f68846eaec0acb
Shailsharma2604 <shailsharma020604@gmail.com>
2024-08-06 10:25:00 +0530

    1

--- W.D/Front-end-development/day_10.js
+++ W.D/Front-end-development/day_10.js
@@ -9,21 +9,45 @@
 
 // console.log(a);
 
-let o=100;     //are created in globe scope and can be accessed in any function
-var l=222;
-const p=333;
-function fun1(){
-    let a=100;
-    console.log("fun1");
-    function fun2(){
-        console.log("fun2");
-        console.log("a",a);
-        console.log("o",o);
-        console.log("l",l);
-        console.log("p",p);
-        // console.log(a);
-    }
-    fun2();
+// let o=100;     //are created in globe scope and can be accessed in any function
+// var l=222;
+// const p=333;
+// function fun1(){
+//     let a=100;
+//     console.log("fun1");
+//     function fun2(){
+//         console.log("fun2");
+//         console.log("a",a);
+//         console.log("o",o);
+//         console.log("l",l);
+//         console.log("p",p);
+//         // console.log(a);
+//     }
+//     fun2();
+// }
+
+// fun1();
+
+
+// function fun1(){
+//     var a=100;
+//     console.log(a);
+// }
+
+// fun1();
+// console.log(a);
+
+
+// {
+//     console.log(a);
+// }
+
+var a=100;
+
+function x (){
+    var a=200;
+    console.log(a);
 }
 
-fun1();
+x();
+console.log(a);
