77f9d920720c53949d469acff0207442accc8c76
Shailsharma2604 <shailsharma020604@gmail.com>
2024-08-07 14:37:20 +0530

    1

---
+++ W.D/Front-end-development/day_11.js
@@ -0,0 +1,30 @@
+
+
+// function one(f){                   //higher order function
+//     console.log("one");
+//     f();
+// }
+
+// one(function(){                    //callback function
+//     console.log("two");
+// });
+
+
+
+
+// function checker(value, sucess, fail){  //higher order function
+//     if(value > 1){
+//         sucess("greater than 1");       //callback function
+//     }else{
+//         fail("less than 1");            //callback function
+//     }
+// }
+
+// checker(10, (msg)=>{
+//     console.log("msg",msg);          //callback function
+// },(msg)=>{
+//     console.log("msg",msg);         //callback function
+// });
+
+
+
