d56be26519f11fcb998821da4616db39c9bc1c28
Shailsharma2604 <shailsharma020604@gmail.com>
2024-08-09 11:15:41 +0530

    1

--- W.D/Front-end-development/day_12.js
+++ W.D/Front-end-development/day_12.js
@@ -18,22 +18,50 @@
 // })
 
 
-let response = fetch('https://jsonplaceholder.typicode.com/posts')
-// console.log(response);
-response.then((data) => {
-    console.log(data);
-    return data.json();
-})
-.then((data) => {
-    console.log(data);
-})
-.catch((error) => {
-    console.log(error);
-})
+// let response = fetch('https://jsonplaceholder.typicode.com/posts')
+// // console.log(response);
+// response.then((data) => {
+//     console.log(data);
+//     return data.json();
+// })
+// // .then((data) => data.json())
+// .then((data) => {
+//     console.log(data);
+// })
+// .catch((error) => {
+//     console.log(error);
+// })
+
+
+// Promise.all([p1, p2, p3]).then((values) => {
+//     console.log(values);
+//     Promise.all([p4, p5, p6]).then((values) => {
+//         console.log(values);
+//     })
+// })
 
 
 
 
 
+// let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+// response.then("res: ",response)
 
-  
+
+
+// async function fetchData() {
+//     let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+//     let data = await response.json()
+//     console.log(data);
+// }
+// fetchData()
+
+
+
+
+let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+let data = await response.json()
+console.log(data);
+
+
+
