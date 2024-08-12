e4171b50e2c8f932f5163fb5ae94692e75209a7a
Shailsharma2604 <shailsharma020604@gmail.com>
2024-08-12 10:37:28 +0530

    1

--- W.D/Front-end-development/day_13.js
+++ W.D/Front-end-development/day_13.js
@@ -30,3 +30,70 @@ one();
 
 console.log(arr.__proto__.__proto__);
 
+Object.prototype == arr.__proto__.__proto__;
+console.log(Object.prototype == arr.__proto__.__proto__);
+
+console.log(Object.__proto__);
+console.log(Object.__proto__.__proto__);
+
+console.log(Object.__proto__ == Function.prototype);
+
+console.log(Array.prototype);
+
+console.log(Array.prototype.__proto__);
+
+console.log(Array.prototype.my = () => {console.log('my function')});
+
+console.log(arr.my());
+
+console.log(Array.prototype.my);
+
+Array.prototype.a=10;
+console.log(arr.a);
+
+let cMAp = function(callback,arr){
+    for(let i=0;i<arr.length;i++){
+        arr[i] = callback(arr[i]);
+    }
+}
+
+
+class MyArray{
+    constructor(){
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index){
+        return this.data[index];
+    }
+
+    push(item){
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+
+    pop(){
+        let lastItem = this.data[this.length-1];
+        delete this.data[this.length-1];
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index){
+        const item = this.data[index];
+        this.shiftItems(index);
+    }
+
+    shiftItems(index){
+        for(let i=index;i<this.length-1;i++){
+            this.data[i] = this.data[i+1];
+        }
+        delete this.data[this.length-1];
+        this.length--;
+    }
+}
+
+
+
