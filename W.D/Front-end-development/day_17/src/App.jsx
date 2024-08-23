337be1aa6c5276f65000baffea6faf1b6267adbf
Shailsharma2604 <shailsharma020604@gmail.com>
2024-08-23 14:43:57 +0530

    1

--- W.D/Front-end-development/day_17/src/App.jsx
+++ W.D/Front-end-development/day_17/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import TestComp from './components/Testcomp'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  // const [count, setCount] = useState(0)
+
+  // const TestComp = () => {
+  //   const [value,setValue]=useState(0)
+  // }
+  //   useEffect(() => {
+  //     console.log('useEffect');
+  //   }, [])
+  //   useEffect(() => {
+  //     console.log('Value changed',value);
+  //   },[value])
+
+  const [value,setValue]=useState(0);
+  const [isVisible,setVisible]=useState(true);
+
 
   return (
     <>
-    
+      <h1>Value:{value}</h1>
+      <button onClick={()=>setValue(value+1)}>Increment</button>
+      {isVisible ==true ? <TestComp />:<></>}
+      <button onClick={()=>setVisible(!isVisible)}>Toggle</button>
     </>
   )
 }
---
+++ W.D/Front-end-development/day_17/src/components/Testcomp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Testcomp() {
+  return (
+    <div>
+      <h1>Component</h1>
+    </div>
+  )
+}
+
+export default Testcomp
