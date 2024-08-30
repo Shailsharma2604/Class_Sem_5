05c21972af518754de01483b3399a5219720d55c
Shailsharma2604 <shailsharma020604@gmail.com>
2024-08-30 14:50:16 +0530

    1

--- W.D/Front-end-development/day_17/src/App.jsx
+++ W.D/Front-end-development/day_17/src/App.jsx
@@ -20,16 +20,16 @@ function App(){
   // const [value,setValue]=useState(0);
   // const [isVisible,setVisible]=useState(true);
 
-  console.log(useRef(0));
-  const ref1 = useRef();
+  // console.log(useRef(0));
+  // const ref1 = useRef();
 
-  return (
-    <>
-      <h1>Value:{ref1.current}</h1>
-      <input type="text" ref={ref1} />
-      {/* <button onClick={()=>ref1.current++}>Increment</button> */}
-    </>
-  )
+  // return (
+  //   <>
+  //     <h1>Value:{ref1.current}</h1>
+  //     <input type="text" ref={ref1} />
+  //     {/* <button onClick={()=>ref1.current++}>Increment</button> */}
+  //   </>
+  // )
 
 
   // return (
@@ -40,6 +40,32 @@ function App(){
   //     <button onClick={()=>setVisible(!isVisible)}>Toggle</button>
   //   </>
   // )
+
+
+
+  const ref1=useRef();
+
+  const handleClick=()=>{
+    ref1.current+=1;
+    console.log(ref1.current);
+  }
+
+  return(
+    <>
+    <h1>app</h1>
+    <h2>Ref1.current:{ref1.current}</h2>
+    <button onClick={handleClick}>Increment</button>
+    <hr/>
+    <h3>Value: {value}</h3>
+    <button onClick={()=>setValue(value+1)}>Increment</button>
+    </>
+  )
+
+
+
+
+
+
 }
 
 export default App
