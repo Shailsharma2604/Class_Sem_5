e0323617caf250184f60a85f6401d439b04d4c27
Shailsharma2604 <shailsharma020604@gmail.com>
2024-11-22 11:08:52 +0530

    1

---
+++ W.D/Back-end-development/new/routers/users.js
@@ -0,0 +1,42 @@
+import {Router} from "express"
+import UserModel from "../models/User"
+const router = Router()
+
+
+router.post("/register",(req,res)=>{
+    try{
+    const email = req.body.email
+    const password = req.body.password
+    const name = req.body.name
+    if(!email||!password||!name){
+        res.status(400).send({message:"Please fill in all fields"})
+    }
+
+    const user =await UserModel.create({
+        email:email,
+        password:password,
+        name:name,
+    })
+    return res.json((user))
+}catch(err){
+    return req.json({msg:"error in user"})
+}
+})
+router.post("/login",(req,res)=>{
+    try{
+        const email = req.body.email
+        const password = req.body.password
+        const admin = await UserModel.findOne({email:email,password:password})
+        if(!admin){
+            return res.status(400).send({message:"Invalid email or password"})
+            }
+        const token =jwt.sign({email:email},JWT_SECRET_USES)
+        return res.send(token)
+    }catch(err){
+        return res.json({msg:"error in admin"})
+    }
+})
+
+
+
+export default router
